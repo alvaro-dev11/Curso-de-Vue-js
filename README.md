a5d5e9df90737098734d5d7149f7a5dfdc957c91
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-06-16 02:41:26 -0500

    Creando componente Contador usando Composition API

--- README.md
+++ README.md
@@ -33,3 +33,7 @@ npm run build
 ```sh
 npm run lint
 ```
+
+## Temas importantes vistos en la documentación
+- API Styles -> Composition API
+- Reactivity Fundamentals
---
+++ src/components/Contador.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const contador = ref(0)
+
+const incrementar = () => {
+  contador.value++
+}
+
+const decrementar = () => {
+  contador.value--
+}
+
+const resetear = () => {
+  contador.value = 0
+}
+
+const cambiarColor = computed(() => {
+  if (contador.value > 0) {
+    return 'green'
+  } else if (contador.value < 0) {
+    return 'red'
+  } else {
+    return 'black'
+  }
+})
+</script>
+
+<template>
+  <p :class="cambiarColor">{{ contador }}</p>
+
+  <button @click="incrementar">Incrementar</button>
+
+  <button @click="decrementar">Decrementar</button>
+
+  <button @click="resetear">Resetear</button>
+</template>
+
+<style scoped>
+.green {
+  color: green;
+}
+
+.red {
+  color: red;
+}
+
+.black {
+  color: black;
+}
+</style>
